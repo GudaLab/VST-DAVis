698f4f89e36a3786c5f7baef8b979efdbd34446a
Guda Lab <118491177+GudaLab@users.noreply.github.com>
2025-04-29 09:49:04 -0500

    Add files via upload

--- scripts/trajectory3.R
+++ scripts/trajectory3.R
@@ -2,8 +2,9 @@ datainput_single_multiple_sample_trajectory3 <- function(index_trajectory3_input
   single_multiple_sample_clustering <- index_trajectory3_input1
   cds <- index_trajectory3_input2
   
-table_deg <- graph_test(cds, neighbor_graph = index_s_trajectory15)
-table_deg <- table_deg %>% arrange(q_value) %>% dplyr::filter(status == "OK")
+table_deg <- graph_test(cds, neighbor_graph = index_s_trajectory15, cores=8)
+#table_deg <- table_deg %>% arrange(q_value) %>% dplyr::filter(status == "OK")
+table_deg <- table_deg %>% arrange(q_value)
 #Add pseudotime values into the seuratobject
 single_multiple_sample_clustering$pseudotime <- pseudotime(cds)
 plots104 <- FeaturePlot(single_multiple_sample_clustering, features = "pseudotime", label=index_s_trajectory16)
